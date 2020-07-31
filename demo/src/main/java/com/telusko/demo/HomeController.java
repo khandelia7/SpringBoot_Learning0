1a9eb0f08ef46c4f71c752d91364e95ea1624dcf
khandelia7 <khandelia7@gmail.com>
2020-07-31 15:15:39 +0530

    Accepting User Input

--- demo/src/main/java/com/telusko/demo/HomeController.java
+++ demo/src/main/java/com/telusko/demo/HomeController.java
@@ -1,5 +1,8 @@
 package com.telusko.demo;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,22 @@ public class HomeController {
 	
 	@RequestMapping("/")
 	public String home() {
-		System.out.println("Home Contoller called");
+		System.out.println("Home Contoller method index is called");
 		return "index.jsp";
+	}
+	
+	@RequestMapping("add")
+	public String add(HttpServletRequest request) {
+		System.out.println("Home Contoller result is called");
+		int num1 = Integer.parseInt(request.getParameter("num1"));
+		int num2 = Integer.parseInt(request.getParameter("num2"));
+		int num3 = num1 + num2; // We have to pass this num3 in result.jsp
+		// Method 1 is Dispatcher servlet
+		//return "result.jsp?num3=" + num3;
+		HttpSession session = request.getSession();
+		session.setAttribute("num3", num3); 
+		// 1st num3 is above one and 2nd num3 is use in jsp file
+		return "result.jsp";
 	}
 
 }
--- demo/src/main/webapp/index.jsp
+++ demo/src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +7,11 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>WELCOME AKASH</h1>
+	<h1>WELCOME AKASH</h1>
+	<form action="add">
+		Enter 1st number: <input type="text" name="num1"> <br>
+		Enter 2nd number: <input type="text" name="num2"> <br> <input
+			type="submit">
+	</form>
 </body>
 </html>
